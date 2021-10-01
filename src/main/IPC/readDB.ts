f724a2a2861d9b8460cb016763496ec0a5f2e54e
Sadra Khosravi <sadrakh@outlook.com>
2021-10-01 11:04:54 -0700

    Update: had to roll back to electron 13.4.0 due to network and other errors in the console

--- src/main/IPC/readDB.ts
+++ src/main/IPC/readDB.ts
@@ -1,4 +1,4 @@
-import { ipcMain } from 'electron';
+import { BrowserWindow, ipcMain } from 'electron';
 import { Experiment, Recording } from '@electron/db/models/index';
 /**
  * All DB read related IPCs
@@ -31,7 +31,22 @@ ipcMain.on('db:get-experiment-settings', async (_event, id) => {
 
 // Get all the recording data
 ipcMain.handle('db:get-recordings', async (_event, data) => {
+  const mainWindow = BrowserWindow.getAllWindows()[0];
+
   console.log(data);
   const myData = await Recording.findAll({ raw: true, limit: 5000 });
-  console.log('Out data', myData);
+  if (myData) {
+    myData.forEach((record: any) => {
+      const data = record.value.split(',');
+      const outputArr = [
+        parseFloat(data[0]),
+        parseFloat(data[1]),
+        parseFloat(data[2]),
+        parseFloat(data[3]),
+        parseFloat(data[4]),
+      ];
+      mainWindow.webContents.send('data:nirs-reader', outputArr);
+      console.log(outputArr);
+    });
+  }
 });
